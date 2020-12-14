1f3d337314de5c328d6a13411e0e4f010e0886cc
Ángela Bonilla <44753691+angelaagmez@users.noreply.github.com>
2020-12-14 12:54:37 +0100

    Update HelloWorld.java

--- eclipse-workspace/Practica_endes/src/practica/HelloWorld.java
+++ eclipse-workspace/Practica_endes/src/practica/HelloWorld.java
@@ -2,7 +2,7 @@ package practica;
 
 public class HelloWorld {
     public static void main(String args[]){
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo desde GitHub");
         System.out.println("Mi nombre es Angela");
     }
 
